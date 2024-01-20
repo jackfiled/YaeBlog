25be0d230256a511676d28c9e4abbaab27f2eb2b
jackfiled <xcrenchangjun@outlook.com>
2024-01-20 17:10:32 +0800

    add: FluentUI主题及其主题框架

---
+++ YaeBlog.Theme.FluentUI/BlogApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using YaeBlog.Core.Builder;
+using YaeBlog.Core.Extensions;
+
+namespace YaeBlog.Theme.FluentUI;
+
+public static class BlogApplicationBuilderExtensions
+{
+    public static void UseFluentTheme(this BlogApplicationBuilder builder)
+    {
+        builder.ConfigureWebApplication(ConfigureWebApplicationBuilder, ConfigureWebApplication);
+    }
+
+    private static void ConfigureWebApplicationBuilder(WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorComponents();
+    }
+
+    private static void ConfigureWebApplication(WebApplication application)
+    {
+        application.UseStaticFiles();
+        application.UseAntiforgery();
+        application.MapRazorComponents<App>();
+    }
+}
