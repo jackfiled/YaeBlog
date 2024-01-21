6bbfd83ef3bf303a116665594a72daca914fe312
jackfiled <xcrenchangjun@outlook.com>
2024-01-21 22:46:12 +0800

    add: 博客脚注

--- YaeBlog.Core/Models/BlogOptions.cs
+++ YaeBlog.Core/Models/BlogOptions.cs
@@ -4,11 +4,33 @@ public class BlogOptions
 {
     public const string OptionName = "Blog";
 
+    /// <summary>
+    /// 博客markdown文件的根目录
+    /// </summary>
     public required string Root { get; set; }
 
+    /// <summary>
+    /// 博客作者
+    /// </summary>
     public required string Author { get; set; }
 
+    /// <summary>
+    /// 博客的起始年份
+    /// </summary>
+    public required int StartYear { get; set; }
+
+    /// <summary>
+    /// 博客项目的名称
+    /// </summary>
     public required string ProjectName { get; set; }
 
+    /// <summary>
+    /// 博客起始页面的背景图片
+    /// </summary>
     public required string BannerImage { get; set; }
+
+    /// <summary>
+    /// 博客底部是否显示ICP备案信息
+    /// </summary>
+    public string? RegisterInformation { get; set; }
 }
