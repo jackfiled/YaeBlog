0d8476a1321b20c6a4e64a01270bd761676ab22d
jackfiled <xcrenchangjun@outlook.com>
2024-07-02 09:17:23 +0800

    feat: 添加个人主页实现 (#1)
    Reviewed-on: https://git.rrricardo.top/jackfiled/YaeBlog/pulls/1

---
+++ YaeBlog/wwwroot/themes.js
@@ -0,0 +1,8 @@
+import * as fluentUI
+    from '/_content/Microsoft.FluentUI.AspNetCore.Components/Microsoft.FluentUI.AspNetCore.Components.lib.module.js';
+
+fluentUI.typeRampBaseFontSize.withDefault("16px");
+fluentUI.typeRampBaseLineHeight.withDefault("16px");
+
+
+
