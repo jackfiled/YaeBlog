c050d1b790fa8f2ed8d67a1802a8d0d21275b175
jackfiled <xcrenchangjun@outlook.com>
2026-01-04 17:21:52 +0800

    feat: add build.ps1 script.

---
+++ YaeBlog/build.ps1
@@ -0,0 +1,90 @@
+#!pwsh
+
+[cmdletbinding()]
+param(
+    [Parameter(Mandatory = $true, Position = 0, HelpMessage = "Specify the build target")]
+    [ValidateSet("tailwind", "watch", "publish", "compress", "build")]
+    [string]$Target,
+    [string]$Output = "wwwroot",
+    [string]$Essay,
+    [switch]$Compress
+)
+
+begin {
+    Write-Host "Building $Target..."
+
+    if ($Target -eq "publish")
+    {
+        if ($Essay -eq "")
+        {
+            Write-Error "No publish target, please add with --essay argument."
+            exit 1
+        }
+    }
+}
+
+process {
+    function Compress-Image
+    {
+        Write-Host "Compress image assets..."
+        dotnet run -- compress --dry-run
+        $confirm = Read-Host "Really compress images? (y/n)"
+        if ($confirm -notmatch "^[yY]$")
+        {
+            Write-Host "Not compress images."
+            return
+        }
+
+        Write-Host "Do compress image..."
+        dotnet run -- compress
+
+        dotnet run -- scan
+        $confirm = Read-Host "Really delete unused images? (y/n)"
+        if ($confirm -notmatch "^[yY]$")
+        {
+            Write-Host "Not delete images."
+            return
+        }
+        Write-Host "Do delete unused images.."
+        dotnet run -- scan --rm
+    }
+
+    function Build-Image
+    {
+        $commitId = git rev-parse --short=10 HEAD
+        dotnet publish
+        podman build . -t ccr.ccs.tencentyun.com/jackfiled/blog --build-arg COMMIT_ID=$commitId
+    }
+
+    switch ($Target)
+    {
+        "tailwind" {
+            Write-Host "Build tailwind css into $Output."
+            pnpm tailwindcss -i wwwroot/tailwind.css -o $Output/tailwind.g.css
+            break
+        }
+        "watch" {
+            dotnet run -- watch
+            break
+        }
+        "publish" {
+            Write-Host "Publish essay $Essay..."
+            dotnet run -- publish $Essay
+
+            if ($Compress)
+            {
+                Compress-Image
+            }
+            break
+        }
+        "compress" {
+            Compress-Image
+            break
+        }
+        "build" {
+            Build-Image
+            break
+        }
+    }
+}
+
