9061092b64c0aa3e2193e2e1f67ccec62e67b4e4
jackfiled <xcrenchangjun@outlook.com>
2026-07-21 23:07:55 +0800

    feat: basic render.

--- src/yaeblog-typst/src/lib.rs
+++ src/yaeblog-typst/src/lib.rs
@@ -1,17 +1,122 @@
 mod ffi_utils;
 mod world;
 
+use crate::ffi_utils::{ExternResult, extract_str, from_handler, into_handler, return_str};
+use crate::world::SystemWorld;
+use anyhow::anyhow;
 use std::ffi::{c_char, c_void};
-use crate::ffi_utils::ExternResult;
+use std::path::PathBuf;
+use std::str::FromStr;
+use typst::diag::{EcoVec, SourceDiagnostic, Warned};
+use typst_html::{HtmlDocument, HtmlOptions};
 
-pub struct TypstCompiler {
+struct TypstCompiler {
+    world: SystemWorld,
+}
 
+impl TypstCompiler {
+    fn new(work_dir: &str) -> anyhow::Result<Self> {
+        let path = PathBuf::from_str(work_dir)?;
+
+        Ok(Self {
+            world: SystemWorld::new(path),
+        })
+    }
+
+    fn compile(&mut self, input: &str) -> anyhow::Result<String> {
+        self.world.set_current_file(input);
+
+        let Warned { output, warnings } = typst::compile::<HtmlDocument>(&mut self.world);
+        let document = match output {
+            Ok(d) => Ok(d),
+            Err(e) => Err(anyhow!(typst_to_err(e))),
+        }?;
+
+        let content = export_html(&document)?;
+        Ok(content)
+    }
+}
+
+fn typst_to_err(e: EcoVec<SourceDiagnostic>) -> String {
+    let mut message = String::new();
+    message.push_str("Failed to compile: \n");
+
+    for err in e {
+        message.push_str(format!("  - {} \n", err.message.as_str()).as_str());
+    }
+
+    message
+}
+
+fn export_html(document: &HtmlDocument) -> anyhow::Result<String> {
+    let options = HtmlOptions { pretty: false };
+
+    match typst_html::html(document, &options) {
+        Ok(o) => Ok(o),
+        Err(e) => Err(anyhow!(typst_to_err(e))),
+    }
+}
+
+fn create_compiler(work_dir: *const c_char) -> anyhow::Result<Box<TypstCompiler>> {
+    let work_dir = extract_str(work_dir)?;
+    let compiler = TypstCompiler::new(work_dir)?;
+
+    Ok(Box::new(compiler))
 }
 
 pub extern "C" fn init_compiler(work_dir: *const c_char) -> ExternResult<*mut c_void> {
-    todo!()
+    let compiler = create_compiler(work_dir);
+    let handler = compiler.map(into_handler);
+
+    ExternResult::from_result(handler)
 }
 
-pub extern "C" fn typst_compile(handler: *mut c_void, str: *const c_char) -> ExternResult<*mut c_char> {
-    todo!()
+fn typst_compile_inner(
+    compiler: &mut TypstCompiler,
+    str: *const c_char,
+) -> anyhow::Result<*mut c_char> {
+    let input = extract_str(str)?;
+    compiler.compile(input).map(return_str)
+}
+
+pub extern "C" fn typst_compile(
+    handler: *mut c_void,
+    str: *const c_char,
+) -> ExternResult<*mut c_char> {
+    let compiler = from_handler::<TypstCompiler>(handler);
+
+    ExternResult::from_result(typst_compile_inner(compiler, str))
+}
+
+pub extern "C" fn free_compiler(handler: *mut c_void) {
+    if handler.is_null() {
+        return;
+    }
+
+    unsafe {
+        // Drop it!
+        let _ = Box::from_raw(handler as *mut TypstCompiler);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn smoke_test() {
+        let mut compiler = TypstCompiler::new(".").unwrap();
+        let result = compiler.compile("= 123").unwrap();
+
+        assert!(result.contains("<h2>123</h2>"))
+    }
+
+    #[test]
+    fn math_test() {
+        let mut compiler = TypstCompiler::new(".").unwrap();
+        let result = compiler.compile(
+            "$1 + 1 = 2$").unwrap();
+
+        assert!(result.contains("math"));
+    }
 }
--- src/yaeblog-typst/src/world.rs
+++ src/yaeblog-typst/src/world.rs
@@ -1,43 +1,127 @@
 use std::path::PathBuf;
 use std::sync::LazyLock;
-use typst::utils::LazyHash;
-use typst::{Library, World};
-use typst::diag::FileResult;
+use typst::diag::{FileError, FileResult, PackageError};
 use typst::foundations::{Bytes, Datetime, Duration};
-use typst::syntax::{FileId, Source};
+use typst::syntax::{FileId, RootedPath, Source, VirtualPath, VirtualRoot};
 use typst::text::{Font, FontBook};
+use typst::utils::LazyHash;
+use typst::{Feature, Features, Library, LibraryExt, World};
+use typst_kit::datetime::Time;
+use typst_kit::files::{FileLoader, FileStore, FsRoot};
+use typst_kit::fonts::{FontStore, embedded};
 
 pub struct SystemWorld {
     work_dir: PathBuf,
     library: LazyHash<Library>,
+    files: FileStore<SystemFiles>,
+    fonts: LazyLock<FontStore, Box<dyn Fn() -> FontStore + Send + Sync>>,
+    now: Time,
+}
+
+const DEFAULT_FEATURES: [Feature; 1] = [Feature::Html];
+
+impl SystemWorld {
+    pub fn new(work_dir: PathBuf) -> Self {
+        let features = DEFAULT_FEATURES.iter().copied().map(Into::into).collect();
+        let library = Library::builder().with_features(features).build();
+
+        Self {
+            work_dir: work_dir.clone(),
+            library: LazyHash::new(library),
+            files: FileStore::new(SystemFiles::new(work_dir)),
+            fonts: LazyLock::new(Box::new(|| {
+                let mut fonts = FontStore::new();
+
+                fonts.extend(embedded());
+
+                fonts
+            })),
+            now: Time::system(),
+        }
+    }
+
+    pub fn set_current_file(&mut self, content: &str) {
+        let current_file = &mut self.files.loader_mut().current_file;
+        let bytes = content.as_bytes();
+
+        current_file.clear();
+        current_file.resize(bytes.len(), 0);
+        current_file.copy_from_slice(content.as_bytes())
+    }
 }
 
 impl World for SystemWorld {
     fn library(&self) -> &LazyHash<Library> {
-        todo!()
+        &self.library
     }
 
     fn book(&self) -> &LazyHash<FontBook> {
-        todo!()
+        self.fonts.book()
     }
 
     fn main(&self) -> FileId {
-        todo!()
+        *MAIN_ID
     }
 
     fn source(&self, id: FileId) -> FileResult<Source> {
-        todo!()
+        self.files.source(id)
     }
 
     fn file(&self, id: FileId) -> FileResult<Bytes> {
-        todo!()
+        self.files.file(id)
     }
 
     fn font(&self, index: usize) -> Option<Font> {
-        todo!()
+        self.fonts.font(index)
     }
 
     fn today(&self, offset: Option<Duration>) -> Option<Datetime> {
-        todo!()
+        self.now.today(offset)
+    }
+}
+
+/// Provides project files from a configured directory and package files from
+/// standard locations.
+struct SystemFiles {
+    pub main: FileId,
+    project: FsRoot,
+    current_file: Vec<u8>,
+}
+
+static MAIN_ID: LazyLock<FileId> = LazyLock::new(|| {
+    FileId::unique(RootedPath::new(
+        VirtualRoot::Project,
+        VirtualPath::new("<main>").unwrap(),
+    ))
+});
+
+impl SystemFiles {
+    pub fn new(work_dir: PathBuf) -> Self {
+        Self {
+            main: *MAIN_ID,
+            project: FsRoot::new(work_dir),
+            current_file: Vec::new(),
+        }
+    }
+
+    pub fn resolve(&self, id: FileId) -> FileResult<PathBuf> {
+        Ok(self.root(id)?.resolve(id.vpath())?)
+    }
+
+    fn root(&self, id: FileId) -> FileResult<FsRoot> {
+        Ok(match id.root() {
+            VirtualRoot::Project => Ok(self.project.clone()),
+            VirtualRoot::Package(_) => Err(FileError::Package(PackageError::Other(None))),
+        }?)
+    }
+}
+
+impl FileLoader for SystemFiles {
+    fn load(&self, id: FileId) -> FileResult<Bytes> {
+        if id == *MAIN_ID {
+            Ok(Bytes::new(self.current_file.clone()))
+        } else {
+            self.root(id)?.load(id.vpath())
+        }
     }
 }
